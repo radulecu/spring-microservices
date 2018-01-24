f3ac38149e1ca0dfc415bcb30546488800339d78
lecu_radu@yahoo.com <tig3r!@#@>
2018-01-24 22:41:10 +0200

    enable ssl over eureka

--- eureka-service/src/main/java/ro/rasel/service/eureka/EurekaServerApplication.java
+++ eureka-service/src/main/java/ro/rasel/service/eureka/EurekaServerApplication.java
@@ -3,13 +3,14 @@ package ro.rasel.service.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import ro.rasel.ssl.truststore.TrustStoreComponent;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(new Class<?>[]{EurekaServerApplication.class, TrustStoreComponent.class}, args);
     }
 
 }
