2af0162653a10cb55b672b0c65264819908c801d
lecu_radu@yahoo.com <tig3r!@#@>
2018-01-24 22:07:58 +0200

    rename classes, pom description

--- contact-service/src/main/java/ro/rasel/service/contacts/config/SecurityConfigurer.java
+++ contact-service/src/main/java/ro/rasel/service/contacts/config/SecurityConfigurer.java
@@ -1,17 +1,16 @@
-package ro.rasel.service.passport.config;
+package ro.rasel.service.contacts.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import ro.rasel.security.client.resource.IResourceSecurityConfigurer;
 
 @Configuration
-public class ResourceSecurityConfigurer implements IResourceSecurityConfigurer {
+public class SecurityConfigurer implements IResourceSecurityConfigurer {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
-                .regexMatcher("/passport/.*")
                 .authorizeRequests()
-                .regexMatchers("/passport/[a-zA-Z0-9]+").access("#oauth2.hasScope('openid')")
+                .regexMatchers("/contacts/[a-zA-Z0-9]+").authenticated()
                 .anyRequest().authenticated();
     }
 }
