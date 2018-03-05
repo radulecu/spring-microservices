e0238ce0db03284aa4e9d023089c3d9d4bbb2b75
lecu_radu@yahoo.com <tig3r!@#@>
2018-03-05 08:06:00 +0200

    eureka blacklist

--- swagger-component/src/main/java/ro/rasel/swagger/SwaggerConfig.java
+++ swagger-component/src/main/java/ro/rasel/swagger/SwaggerConfig.java
@@ -1,10 +1,10 @@
 package ro.rasel.swagger;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import springfox.documentation.builders.PathSelectors;
@@ -16,23 +16,25 @@ import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Configuration
 @EnableSwagger2
+@ComponentScan
 public class SwaggerConfig {
+    private SwaggerConfigProperties swaggerConfigProperties;
 
-    @Value("${swagger.enabled:false}")
-    boolean swaggerEnabled;
-
-    @Value("${spring.application.name:${vcap.application.name:${spring.config.name:application}}}")
-    private String name = "${spring.application.name:${vcap.application.name:${spring.config.name:application}}}";
+    @Autowired
+    public SwaggerConfig(SwaggerConfigProperties swaggerConfigProperties) {
+        this.swaggerConfigProperties = swaggerConfigProperties;
+    }
 
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .enable(swaggerEnabled)
+                .enable(swaggerConfigProperties.isEnabled())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("ro.rasel"))
                 .paths(PathSelectors.any())
@@ -53,13 +55,15 @@ public class SwaggerConfig {
 
         @Override
         public List<SwaggerResource> get() {
+            Set<String> blacklist = swaggerConfigProperties.getBlacklist();
             List<SwaggerResource> collect =
-                    discoveryClient.getServices().stream().filter(s -> s != null && !s.equals(name)).map(s -> {
-                        SwaggerResource swaggerResource = new SwaggerResource();
-                        swaggerResource.setName(s);
-                        swaggerResource.setLocation("/" + s + "/v2/api-docs");
-                        return swaggerResource;
-                    }).collect(Collectors.toList());
+                    discoveryClient.getServices().stream().filter(s -> s != null && !blacklist.contains(s))
+                            .map(s -> {
+                                SwaggerResource swaggerResource = new SwaggerResource();
+                                swaggerResource.setName(s);
+                                swaggerResource.setLocation("/" + s + "/v2/api-docs");
+                                return swaggerResource;
+                            }).collect(Collectors.toList());
             return collect;
         }
     }
