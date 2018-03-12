4000578cffb4647571e7fdf87e8d60da9703be46
lecu_radu@yahoo.com <tig3r!@#@>
2018-03-12 07:40:26 +0200

    eureka security

--- eureka-service/src/main/java/ro/rasel/service/eureka/EurekaServerApplication.java
+++ eureka-service/src/main/java/ro/rasel/service/eureka/EurekaServerApplication.java
@@ -3,8 +3,6 @@ package ro.rasel.service.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import ro.rasel.security.client.resource.ResourceSecurityClientComponent;
-import ro.rasel.security.client.sso.WebSecurityClientComponent;
 import ro.rasel.service.bookmarks.EurekaClientComponent;
 import ro.rasel.ssl.truststore.TrustStoreComponent;
 
@@ -14,8 +12,7 @@ public class EurekaServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(new Class<?>[]{EurekaServerApplication.class, EurekaClientComponent.class,
-                TrustStoreComponent.class, ResourceSecurityClientComponent.class,
-                WebSecurityClientComponent.class}, args);
+                TrustStoreComponent.class}, args);
     }
 
 }
