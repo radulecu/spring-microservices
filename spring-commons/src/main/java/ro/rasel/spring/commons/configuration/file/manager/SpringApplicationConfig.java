9dd9269c0e57bbaaa22c46993cbcab34db8233fb
lecu-radu <lecu-radu@gmail.com>
2018-06-12 00:55:48 +0300

    use interface for configuration property class

--- spring-commons/src/main/java/ro/rasel/spring/commons/configuration/file/manager/SpringApplicationConfig.java
+++ spring-commons/src/main/java/ro/rasel/spring/commons/configuration/file/manager/SpringApplicationConfig.java
@@ -1,29 +1,9 @@
 package ro.rasel.spring.commons.configuration.file.manager;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
 import java.io.File;
 
-@Configuration
-@ConfigurationProperties("spring.application")
-public class SpringApplicationConfig {
-    private File tempDir;
-    private String group="spring";
+public interface SpringApplicationConfig {
+    File getTempDir();
 
-    public File getTempDir() {
-        return tempDir;
-    }
-
-    public void setTempDir(File tempDir) {
-        this.tempDir = tempDir;
-    }
-
-    public String getGroup() {
-        return group;
-    }
-
-    public void setGroup(String group) {
-        this.group = group;
-    }
+    String getGroup();
 }
