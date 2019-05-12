639535f58118b42e899176985cb3020160437479
Lecu Radu <lecu.radu@gmail.com>
2019-05-12 23:38:28 +0300

    add disable mechanism for security

--- security-sso-component/src/main/java/ro/rasel/security/client/sso/WebSecurityClientComponent.java
+++ security-sso-component/src/main/java/ro/rasel/security/client/sso/WebSecurityClientComponent.java
@@ -4,12 +4,13 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import ro.rasel.security.client.common.SecurityClientComponent;
 import ro.rasel.security.client.common.config.OAuth2FeignAutoConfiguration;
 
 @ConditionalOnProperty(name = "security.enabled", havingValue = "true", matchIfMissing = true)
 @ComponentScan(basePackageClasses = {WebSecurityClientComponent.class, OAuth2FeignAutoConfiguration.class})
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @PropertySource("classpath:application-security.properties")
-public class WebSecurityClientComponent {
+public class WebSecurityClientComponent implements SecurityClientComponent {
 
 }
