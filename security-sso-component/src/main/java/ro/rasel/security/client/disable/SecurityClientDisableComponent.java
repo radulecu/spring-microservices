562df18bcc57012178d8865d1a10e3e7fac97d41
Lecu Radu <lecu.radu@gmail.com>
2019-06-09 15:46:25 +0300

    fix security disabled

--- security-sso-component/src/main/java/ro/rasel/security/client/disable/SecurityClientDisableComponent.java
+++ security-sso-component/src/main/java/ro/rasel/security/client/disable/SecurityClientDisableComponent.java
@@ -4,10 +4,10 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import ro.rasel.security.client.common.SecurityClientComponent;
-import ro.rasel.security.client.disable.config.SecurityClientComponentDisableConfig;
+import ro.rasel.security.client.disable.config.SecurityClientDisableConfigurer;
 
 @Configuration
-@Import(SecurityClientComponentDisableConfig.class)
+@Import(SecurityClientDisableConfigurer.class)
 @ConditionalOnMissingBean(SecurityClientComponent.class)
-public class SecurityClientComponentDisable {
+public class SecurityClientDisableComponent {
 }
--- security-sso-component/src/main/java/ro/rasel/security/client/disable/config/SecurityClientDisableConfigurer.java
+++ security-sso-component/src/main/java/ro/rasel/security/client/disable/config/SecurityClientDisableConfigurer.java
@@ -3,11 +3,13 @@ package ro.rasel.security.client.disable.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@Order(-1)
-public class SecurityClientComponentDisableConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+@Order(10)
+public class SecurityClientDisableConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
