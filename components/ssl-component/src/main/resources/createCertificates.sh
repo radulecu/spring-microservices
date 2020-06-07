cdf174124add3e3edcd482280a8db73c2452463f
Radu Lecu <lecu.radu@gmail.com>
2020-06-07 13:47:02 +0300

    change key-alias name

--- components/ssl-component/src/main/resources/createCertificates.sh
+++ components/ssl-component/src/main/resources/createCertificates.sh
@@ -1,6 +1,6 @@
-del *.jks
-del *.cer
+rm *.jks
+rm *.cer
 
-keytool -genkey -alias localhost -keyalg RSA -keystore localhost-identity.jks -deststoretype JKS -validity 1825 -keypass jkspass -storepass jkspass -dname "CN=localhost, OU=N/A, O=N/A, L=Bucharest, ST=Romania, C=RO correct?"
-keytool -export -alias localhost -keystore localhost-identity.jks -file localhost.cer -storepass jkspass
+keytool -genkey -alias keyalias -keyalg RSA -keystore localhost-identity.jks -deststoretype JKS -validity 1825 -keypass jkspass -storepass jkspass -dname "CN=localhost, OU=N/A, O=N/A, L=Bucharest, ST=Romania, C=RO correct?"
+keytool -export -alias keyalias -keystore localhost-identity.jks -file localhost.cer -storepass jkspass
 keytool -importcert -keystore truststore.jks -alias localhost -storepass jkspass -file localhost.cer -noprompt
