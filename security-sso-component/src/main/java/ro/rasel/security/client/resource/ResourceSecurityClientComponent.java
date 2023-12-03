164348c8783a447a6e94a6924ab312af7934e979
Radu Lecu <lecu.radu@gmail.com>
2023-12-04 00:28:51 +0200

    cleanup

--- security-sso-component/src/main/java/ro/rasel/security/client/resource/ResourceSecurityClientComponent.java
+++ security-sso-component/src/main/java/ro/rasel/security/client/resource/ResourceSecurityClientComponent.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import ro.rasel.security.client.configuration.OAuth2FeignAutoConfiguration;
 
-@ConditionalOnProperty(value = "security.enabled", matchIfMissing = false)
+@ConditionalOnProperty(value = "security.enabled")
 @ComponentScan(basePackageClasses = {ResourceSecurityClientComponent.class, OAuth2FeignAutoConfiguration.class})
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceSecurityClientComponent {
