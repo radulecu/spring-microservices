dac88fac942053d8bfd8aafe38a8789582c75b11
Radu Lecu <lecu.radu@gmail.com>
2023-12-04 00:29:17 +0200

    refactor security

--- security-sso-auth-server/src/main/java/ro/rasel/server/security/dao/UserDetailsJDBCDao.java
+++ security-sso-auth-server/src/main/java/ro/rasel/server/security/dao/UserDetailsJDBCDao.java
@@ -6,9 +6,13 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class UserDetailsJDBCDao implements UserDetailsDao {
 
+    public static final String GET_USER_SQL = "SELECT * FROM users WHERE user_name = ?";
+    public static final String GET_USER_ROLES_SQL = "SELECT role FROM user_roles WHERE user_name = ?";
     private final JdbcTemplate jdbcTemplate;
 
     UserDetailsJDBCDao(JdbcTemplate jdbcTemplate) {
@@ -17,17 +21,20 @@ public class UserDetailsJDBCDao implements UserDetailsDao {
 
     @Override
     public User findUserByName(String username) {
+        List<String> roles = jdbcTemplate.queryForList(GET_USER_ROLES_SQL, String.class,
+                username);
+
         RowMapper<User> userDetailsRowMapper = (rs, i) -> new User(
-                rs.getString("ACCOUNT_NAME"),
-                rs.getString("PASSWORD"),
-                rs.getBoolean("ENABLED"),
-                rs.getBoolean("ENABLED"),
-                rs.getBoolean("ENABLED"),
-                rs.getBoolean("ENABLED"),
-                AuthorityUtils.createAuthorityList("ROLE_USER", "ROLE_ADMIN"));
+                rs.getString("user_name"),
+                rs.getString("password"),
+                rs.getBoolean("enabled"),
+                rs.getBoolean("enabled"),
+                rs.getBoolean("enabled"),
+                rs.getBoolean("enabled"),
+                AuthorityUtils.createAuthorityList(roles.toArray(new String[roles.size()])));
 
         return jdbcTemplate.queryForObject(
-                "select * from ACCOUNT where ACCOUNT_NAME = ?", userDetailsRowMapper,
+                GET_USER_SQL, userDetailsRowMapper,
                 username);
     }
 }
--- security-sso-auth-server/src/main/resources/schema.sql
+++ security-sso-auth-server/src/main/resources/schema.sql
@@ -1,16 +1,20 @@
-DROP TABLE account if EXISTS ;
-DROP TABLE client_details if EXISTS ;
+DROP TABLE users if EXISTS ;
+DROP TABLE user_roles if EXISTS ;
 
-create table account ( ACCOUNT_NAME varchar(255) not null,
-                      PASSWORD varchar(255 ) not null,
-                      ID INTEGER,
-                      ENABLED BOOLEAN default true)  ;
+CREATE TABLE users(
+  user_name varchar(255) not null,
+  password varchar(255) not null,
+  enabled BOOLEAN default true);
 
-create table client_details(
-  CLIENT_ID VARCHAR (255) not null unique ,
-  CLIENT_SECRET VARCHAR (255) not null   ,
-  RESOURCE_IDS VARCHAR (255) null ,
-  SCOPES VARCHAR (255) null ,
-  GRANT_TYPES VARCHAR (255) null ,
-  AUTHORITIES VARCHAR (255) null
-);
+CREATE TABLE user_roles(
+  user_name varchar(255) not null,
+  role varchar(255) not null,);
+
+-- create table client_details(
+--   CLIENT_ID VARCHAR (255) not null unique ,
+--   CLIENT_SECRET VARCHAR (255) not null   ,
+--   RESOURCE_IDS VARCHAR (255) null ,
+--   SCOPES VARCHAR (255) null ,
+--   GRANT_TYPES VARCHAR (255) null ,
+--   AUTHORITIES VARCHAR (255) null
+-- );
