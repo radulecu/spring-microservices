02044ff0a53a44753c8a0426a90b71b1cf9e1cc2
Radu Lecu <lecu.radu@gmail.com>
2023-12-04 00:29:54 +0200

    eureka security

--- eureka-service/src/main/java/ro/rasel/service/eureka/config/ResourceSecurityConfigurer.java
+++ eureka-service/src/main/java/ro/rasel/service/eureka/config/ResourceSecurityConfigurer.java
@@ -1,20 +1,20 @@
-package ro.rasel.service.eureka.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import ro.rasel.security.client.resource.IResourceSecurityConfigurer;
-
-@Configuration
-public class ResourceSecurityConfigurer implements IResourceSecurityConfigurer {
-    static final String REGEX_EUREKA_PATTERN = "/|/eureka";
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http
-            .regexMatcher(REGEX_EUREKA_PATTERN)
-            .authorizeRequests()
-                .anyRequest().authenticated();
-        // @formatter:on
-    }
-}
+//package ro.rasel.service.eureka.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import ro.rasel.security.client.resource.IResourceSecurityConfigurer;
+//
+////@Configuration
+//public class ResourceSecurityConfigurer implements IResourceSecurityConfigurer {
+//    static final String REGEX_EUREKA_PATTERN = "/|/eureka";
+//
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        // @formatter:off
+//        http
+//            .regexMatcher(REGEX_EUREKA_PATTERN)
+//            .authorizeRequests()
+//                .anyRequest().authenticated();
+//        // @formatter:on
+//    }
+//}
--- eureka-service/src/main/java/ro/rasel/service/eureka/config/WebSecurityConfigurer.java
+++ eureka-service/src/main/java/ro/rasel/service/eureka/config/WebSecurityConfigurer.java
@@ -1,21 +1,21 @@
-package ro.rasel.service.eureka.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import ro.rasel.security.client.sso.IWebSecurityConfigurer;
-
-@Configuration
-public class WebSecurityConfigurer implements IWebSecurityConfigurer {
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http.regexMatcher(regetNot(ResourceSecurityConfigurer.REGEX_EUREKA_PATTERN))
-            .authorizeRequests()
-                .anyRequest().authenticated();
-        // @formatter:on
-    }
-
-    private String regetNot(String s) {
-        return "(?!" + s + ").*";
-    }
-}
+//package ro.rasel.service.eureka.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import ro.rasel.security.client.sso.IWebSecurityConfigurer;
+//
+//@Configuration
+//public class WebSecurityConfigurer implements IWebSecurityConfigurer {
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        // @formatter:off
+//        http.regexMatcher(regetNot(ResourceSecurityConfigurer.REGEX_EUREKA_PATTERN))
+//            .authorizeRequests()
+//                .anyRequest().authenticated();
+//        // @formatter:on
+//    }
+//
+//    private String regetNot(String s) {
+//        return "(?!" + s + ").*";
+//    }
+//}
