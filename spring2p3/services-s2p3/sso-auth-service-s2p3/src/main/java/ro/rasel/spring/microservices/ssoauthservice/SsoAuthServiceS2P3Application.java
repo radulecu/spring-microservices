4966bce866c140037f2282796a1f90b860bf3576
Radu Lecu <lecu.radu@gmail.com>
2023-12-04 00:22:10 +0200

    freeze sso auth service on spring 2.3 (new variant should be created), hystrix service and gateway service and rename resource gateway service

---
+++ spring2p3/services-s2p3/sso-auth-service-s2p3/src/main/java/ro/rasel/spring/microservices/ssoauthservice/SsoAuthServiceS2P3Application.java
@@ -0,0 +1,18 @@
+package ro.rasel.spring.microservices.ssoauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import ro.rasel.spring.microservices.component.ssl.server.EnableSslServerComponent;
+import ro.rasel.spring.microservices.component.swagger.EnableSwaggerComponent;
+
+@SpringBootApplication
+@EnableSslServerComponent
+@EnableSwaggerComponent
+@EnableResourceServer
+public class SsoAuthServiceS2P3Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SsoAuthServiceS2P3Application.class, args);
+    }
+
+}
