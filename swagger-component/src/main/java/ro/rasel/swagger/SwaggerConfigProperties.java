fb91a72d5b939da50dfd5f7afc7084c6409b8dc6
Radu Lecu <lecu.radu@gmail.com>
2023-12-04 00:29:41 +0200

    eureka blacklist

---
+++ swagger-component/src/main/java/ro/rasel/swagger/SwaggerConfigProperties.java
@@ -0,0 +1,30 @@
+package ro.rasel.swagger;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Set;
+
+@Configuration
+@ConfigurationProperties("swagger")
+public class SwaggerConfigProperties {
+    private boolean enabled=false;
+
+    private Set<String> blacklist;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<String> getBlacklist() {
+        return blacklist;
+    }
+
+    public void setBlacklist(Set<String> blacklist) {
+        this.blacklist = blacklist;
+    }
+}
