c48e90e6b41b4b1da2bc976c1f86d055a8a943f2
Radu Lecu <lecu.radu@gmail.com>
2023-12-04 00:29:50 +0200

    app refactoring

--- security-sso-component/src/main/java/ro/rasel/security/client/sso/WebSecurityClientComponent.java
+++ security-sso-component/src/main/java/ro/rasel/security/client/sso/WebSecurityClientComponent.java
@@ -4,11 +4,11 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import ro.rasel.security.client.configuration.OAuth2FeignAutoConfiguration;
-import ro.rasel.spring.commons.SpringCommonPackageRoot;
+import ro.rasel.spring.commons.SpringCommonComponent;
 
 @ConditionalOnProperty(name = "security.enabled", havingValue = "true")
 @ComponentScan(basePackageClasses = {WebSecurityClientComponent.class, OAuth2FeignAutoConfiguration.class,
-        SpringCommonPackageRoot.class})
+        SpringCommonComponent.class})
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityClientComponent {
 
